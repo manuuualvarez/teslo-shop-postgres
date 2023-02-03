eea234422ef8355832f2fd939d8ef927889e591e
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-02-03 06:18:10 -0300

    add a querybuilder for search by id and by slug on products endpoint

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { validate as isUUID } from 'uuid'
 
 @Injectable()
 export class ProductsService {
@@ -35,12 +36,21 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
-    if (!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+  async findOne(term: string) {
+    let product: Product
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+          .where('LOWER(title)= :title or slug =:slug', {
+            title: term.toLowerCase(),
+            slug: term.toLowerCase(),
+          }).getOne();
     }
-
+    if (!product) {
+      throw new NotFoundException(`Product with  ${ term } not found`);
+    }
     return product;
   }
 
