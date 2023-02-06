b97bbb6885b4bbe65af95cc4de832a12f57e29e7
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-02-06 06:27:27 -0300

    create a signup endpoint

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { BadRequestException, ConsoleLogger, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+    ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+
+      await this.userRepository.save(user);
+      return user;
+
+    } catch (error) {
+      this.handleDBError(error);
+    }
+  }
+
+  private handleDBError(error: any): never {
+    if(error.code === '23505') 
+      throw new BadRequestException(error.detail);
+    
+      console.log('error', error);
+
+      throw new InternalServerErrorException('Please check logs')
+      
+  }
+}
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    uid: string;
+    
+    @Column('text', {
+        unique: true,
+    })
+    email: string;
+    
+    @Column('text')
+    password: string;
+
+    @Column('text')
+    firstName: string;
+
+    @Column('text')
+    lastName: string;
+
+    @Column('bool', {
+        default: true
+    })
+    isActive: boolean;
+
+    @Column('text', {
+        array: true,
+        default: ['user']
+    })
+    role: string[];
+    
+}
