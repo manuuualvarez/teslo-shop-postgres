36277fac202474041cb58441864c740fb287b667
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-02-16 06:06:56 -0300

    export the custom decorator about Auth to seed module

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { ValidUserRoles } from 'src/auth/interfaces/valid-user-roles';
 
 @Controller('seed')
 export class SeedController {
@@ -7,6 +9,7 @@ export class SeedController {
 
 
   @Get()
+  @Auth(ValidUserRoles.superUser)
   executeSeed() {
     return this.seedService.runSeed();
   }
