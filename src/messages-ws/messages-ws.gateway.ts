5c283991f386e96a8c13adaf4fb04eef622c5809
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-03-13 06:04:22 -0300

    connected to socket

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,17 @@
+import { WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway(8080,{ cors: true })
+export class MessagesWsGateway implements  OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+    ) {}
+
+  handleConnection(client: Socket) {
+    this.messagesWsService.registerClient(client);
+  }
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client.id);
+  }
+}
---
+++ src/messages-ws/messages-ws.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+
+interface ConnectedClients {
+    [id: string]: Socket
+}
+
+
+@Injectable()
+export class MessagesWsService {
+    private connectClients: ConnectedClients = {};
+
+    registerClient(client: Socket) {
+        this.connectClients[client.id] = client;
+        // console.log(`Client ${client.id} connected. Total clients: ${this.getConnectedClients()}`);
+    }
+
+    removeClient(clientId: string) {
+        delete this.connectClients[clientId];
+    }
+
+    getConnectedClients(): number {
+        return Object.keys(this.connectClients).length;
+    }
+}
